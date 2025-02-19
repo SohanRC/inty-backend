cb93d5934e26e7aca186f158ae3d4d9dbfadc3d3
saiyam0211 <saiyamkumar2007@gmail.com>
2025-02-19 11:55:01 +0530

    .

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ connectDB();
 // Middleware
 app.use(cors({
     origin: [
-      'http://localhost:3000',  // Local development
+      'http://localhost:5173',  // Local development
       'https://inty-2qxy.vercel.app',  // Your specific Vercel deployment
       // Add any other domains you're using
     ],
